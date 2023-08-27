deb02af74459c6003762c8bd4cbc361ff75ff4a1
Willian Fernandes <willian@willianfernandes.com.br>
2023-08-27 13:03:28 -0300

    Add code linting support with Eslint

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "plugins" },
   },
   defaults = {
