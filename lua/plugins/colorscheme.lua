35ac2d8a678b255cbe93a05e512a841db0b89d5c
Willian Fernandes <willian@willianfernandes.com.br>
2023-12-08 11:01:44 -0300

    Update catppuccin config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,12 +5,55 @@ return {
     priority = 1000,
     ---@class CatppuccinOptions
     opts = {
+      flavour = "mocha",
       transparent_background = true,
       custom_highlights = function(colors)
         return {
           PackageInfoOutdatedVersion = { fg = colors.peach },
+          CurSearch = { bg = "#FFCA27" },
         }
       end,
+      integrations = {
+        cmp = true,
+        fidget = true,
+        gitsigns = true,
+        harpoon = true,
+        lsp_trouble = true,
+        mason = true,
+        neotest = true,
+        noice = true,
+        notify = true,
+        octo = true,
+        telescope = {
+          enabled = true,
+        },
+        treesitter = true,
+        treesitter_context = false,
+        symbols_outline = true,
+        illuminate = true,
+        which_key = true,
+        barbecue = {
+          dim_dirname = true,
+          bold_basename = true,
+          dim_context = false,
+          alt_background = false,
+        },
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+        },
+      },
     },
   },
   {
