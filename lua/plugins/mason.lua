c75e94a9140287f8ed03dd8417cced85a9028247
Willian Fernandes <willian@willianfernandes.com.br>
2023-12-08 12:02:39 -0300

    Update mason config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,8 +1,17 @@
 return {
   "williamboman/mason.nvim",
   opts = {
+    ui = {
+      border = "rounded",
+      icons = {
+        package_installed = " ",
+        package_pending = "󰌚 ",
+        package_uninstalled = "󰢤 ",
+      },
+    },
     ensure_installed = {
       "emmet-language-server",
+      "graphql-language-service-cli",
     },
   },
 }
