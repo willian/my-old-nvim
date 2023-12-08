5617314bd961a40bb257ab750e2a60ab8b7bee83
Willian Fernandes <willian@willianfernandes.com.br>
2023-12-08 11:39:13 -0300

    Install dnlhc/glance.nvim

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,24 @@
+return {
+  "neovim/nvim-lspconfig",
+  ---@class PluginLspOpts
+  opts = {
+    servers = {
+      yamlls = {
+        settings = {
+          yaml = { keyOrdering = false },
+        },
+      },
+      graphql = {
+        filetypes = { "graphql", "typescript", "typescriptreact" },
+      },
+    },
+  },
+  init = function()
+    -- NOTE: https://www.lazyvim.org/plugins/lsp#%EF%B8%8F-customizing-lsp-keymaps
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    keys[#keys + 1] = { "gD", "<CMD>Glance definitions<CR>" }
+    keys[#keys + 1] = { "gM", "<CMD>Glance implementations<CR>" }
+    keys[#keys + 1] = { "gR", "<CMD>Glance references<CR>" }
+    keys[#keys + 1] = { "gY", "<CMD>Glance type_definitions<CR>" }
+  end,
+}
